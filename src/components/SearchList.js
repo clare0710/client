2ec5933d0488e6030620f898c503b3e70b2f7a28
clare0710 <lisanwan710@gmail.com>
2023-01-04 11:05:15 +0800

    Add profile page and popups

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -10,6 +10,13 @@ import { FaPlusSquare } from 'react-icons/fa';
 import DropDownBtn from './DropDownBtn';
 import SpotifyPlayer from "react-spotify-web-playback";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./DropDownBtn.css";
+import VerticalMenu from "../img/vertical_menu.svg";
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+
+import Popup from './Popup'
+import "./Popup.css"
 
 function SearchList() {
     const [searchKey, setSearchKey] = useState("")
@@ -18,6 +25,16 @@ function SearchList() {
     const [toptracks, setTopTracks] = useState([])
     const [playingTrack, setPlayingTrack] = useState()
     const [play, setPlay] = useState(false)
+
+    const [buttonPopup, setButtonPopup] = useState(false)
+    const [buttonPopup1, setButtonPopup1] = useState(false)
+    const [buttonPopup2, setButtonPopup2] = useState(false)
+    const [viewArtists, setViewArtists] = useState([])
+    const [viewAlbum, setViewAlbum] = useState([])
+    const [trackId, setTrackId] = useState([])
+    const [viewArtistsList, setViewArtistsList] = useState([])
+    const [viewAlbumList, setViewAlbumList] = useState([])
+    const [viewRecList, setViewRecList] = useState([])
 
     useEffect(() => setPlay(true), [playingTrack])
 
@@ -33,6 +50,7 @@ function SearchList() {
                 limit: 10
             }
         })
+        console.log(data)
         setTracks(data.tracks.items)
     }
 
@@ -52,7 +70,21 @@ function SearchList() {
                     <div><audio src={track.preview_url} className="audio" controls>
                     </audio></div>
                     <div><Button className="listen-button" onClick={() => setPlayingTrack(track.uri)} >Listen</Button></div>
-                    <DropDownBtn />
+                    <DropdownButton variant="outline-light" className="vertical-menu-button"
+
+                        title={
+                            <img className='vertical-menu-icon' alt='vertical_menu_icon' src={VerticalMenu} />
+                        }
+                        onClick={() => {
+                            setViewAlbum(track.album.id);
+                            setViewArtists(track.artists[0].id);
+                            setTrackId(track.id)
+                        }}
+                    >
+                        <Dropdown.Item onClick={() => { setButtonPopup(true); ViewAlbum() }}>View album</Dropdown.Item>
+                        <Dropdown.Item onClick={() => { setButtonPopup1(true); ViewArtist() }}>View artist</Dropdown.Item>
+                        <Dropdown.Item onClick={() => { setButtonPopup2(true); ViewRec() }}>Recommended song</Dropdown.Item>
+                    </DropdownButton>
                 </ListGroup.Item>
             </ListGroup>
         ))
@@ -72,18 +104,18 @@ function SearchList() {
         })
         console.log(data)
         setArtists(data.artists.items)
-        // const { id } = data.artists.items[0].id
-        // console.log("id", id)
-        // const { data2 } = await axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks`, {
-        //     headers: {
-        //         Authorization: `Bearer ${Cookies.get('access_token')}`
-        //     },
-        //     params: {
-        //         market: "TW"
-        //     }
-        // })
-        // console.log("data2", data2)
-        // setTopTracks(data2.tracks)
+        const { id } = data.artists.items[0].id
+        console.log("id", id)
+        const { data2 } = await axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks`, {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            },
+            params: {
+                market: "TW"
+            }
+        })
+        console.log("data2", data2)
+        setTopTracks(data2.tracks)
     }
 
     const renderArtists = () => {
@@ -111,19 +143,19 @@ function SearchList() {
 
     const TopTracks = async (e) => {
         e.preventDefault()
-        // const data = artists.map(async artist => {
-        const d = await axios.get(`https://api.spotify.com/v1/artists/${artists[0].id}/top-tracks`, {
-            headers: {
-                Authorization: `Bearer ${Cookies.get('access_token')}`
-            },
-            params: {
-                market: "TW"
-            }
-        })
+        const data = artists.map(async artist => {
+            const d = await axios.get(`https://api.spotify.com/v1/artists/${artists[0].id}/top-tracks`, {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('access_token')}`
+                },
+                params: {
+                    market: "TW"
+                }
+            })
 
-        setTopTracks(d.data.tracks) //set first aritst's top tracks
-        console.log(d.data.tracks)
-        // })
+            setTopTracks(d.data.tracks) //set first aritst's top tracks
+            console.log(d.data.tracks)
+        })
     }
 
     const renderTopTracks = () => {  //can only render first aritst's top tracks
@@ -146,6 +178,97 @@ function SearchList() {
         ))
     }
 
+    const ViewAlbum = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get(`https://api.spotify.com/v1/albums/${viewAlbum}/tracks`, {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            }
+
+        })
+        console.log(data)
+        setViewAlbumList(data.items)
+    }
+
+    const renderViewAlbum = () => {
+        return viewAlbumList.map((track, index) => (
+            <ListGroup>
+                <ListGroup.Item
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="number-horizontal-center">{index + 1}.</div>
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold information-horizontal-center">{track.name}</div>
+                    </div>
+                    <div><Button className="listen-button" onClick={() => setPlayingTrack(track.uri)} >Listen</Button></div>
+                </ListGroup.Item>
+            </ListGroup>
+        ))
+    }
+
+    const ViewArtist = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get(`https://api.spotify.com/v1/artists/${viewArtists}/top-tracks`, {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            },
+            params: {
+                market: "TW"
+            }
+        })
+        console.log(data.tracks)
+        setViewArtistsList(data.tracks)
+    }
+    const renderViewArtists = () => {
+        return viewArtistsList.map((track, index) => (
+            <ListGroup>
+                <ListGroup.Item
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="number-horizontal-center">{index + 1}</div>
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold information-horizontal-center">{track.name}</div>
+                    </div>
+                    <div><Button className="listen-button" onClick={() => setPlayingTrack(track.uri)} >Listen</Button></div>
+                </ListGroup.Item>
+            </ListGroup>
+        ))
+    }
+    const ViewRec = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get("https://api.spotify.com/v1/recommendations", {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            },
+            params: {
+                seed_artists: viewArtists,
+                seed_genres: "pop",
+                seed_tracks: trackId,
+                limit: 5,
+                market: "TW"
+            }
+        })
+        console.log(data)
+        setViewRecList(data.tracks)
+    }
+    const renderViewRec = () => {
+        return viewRecList.map((track, index) => (
+            <ListGroup>
+                <ListGroup.Item
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="number-horizontal-center">{index + 1}</div>
+                    <div><img src={track.album.images[2].url} className="image" alt='album_image' /></div>
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold information-horizontal-center">{track.name}</div>
+                        <div className="information-horizontal-center2">{track.artists[0].name}</div>
+                    </div>
+                    <div><Button className="listen-button" onClick={() => setPlayingTrack(track.uri)} >Listen</Button></div>
+                </ListGroup.Item>
+            </ListGroup>
+        ))
+    }
+
     return (
         <div >
             <SpotifyPlayer
@@ -161,19 +284,41 @@ function SearchList() {
                 <Form.Group>
                     <Form.Control className="search-box" placeholder="Search by song or artist..." onChange={e => setSearchKey(e.target.value)} />
                 </Form.Group>
-                <Button type="submit" className="search-song-button" onClick={searchTracks}>Search</Button>
+                <Button type="submit" className="search-song-button" onClick={searchTracks}>Songs</Button>
                 {/* <Button type="submit" className="search-artist-button" onClick={searchArtists}>Artists</Button> */}
 
                 <ListGroup className="search-list">
                     {renderTracks()}
                 </ListGroup>
-                {/* <ListGroup className="search-list2">
+                {/*  <ListGroup className="search-list2">
                     {renderArtists()}
                 </ListGroup>
                 <ListGroup className="search-list3">
                     {renderTopTracks()}
-                </ListGroup> */}
+                </ListGroup>  */}
             </Form>
+
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                <ListGroup>
+                    <br></br>
+                    <br></br>
+                    {renderViewAlbum()}
+                </ListGroup>
+            </Popup>
+            <Popup trigger={buttonPopup1} setTrigger={setButtonPopup1}>
+                <ListGroup>
+                    <br></br>
+                    <br></br>
+                    {renderViewArtists()}
+                </ListGroup>
+            </Popup>
+            <Popup trigger={buttonPopup2} setTrigger={setButtonPopup2}>
+                <ListGroup>
+                    <br></br>
+                    <br></br>
+                    {renderViewRec()}
+                </ListGroup>
+            </Popup>
 
         </div>
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,140 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import Cookies from 'js-cookie';
+import Appbar from "../components/Appbar";
+import "../components/Appbar.js";
+import axios from 'axios';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FaPlusSquare } from 'react-icons/fa';
 
 const Profile = () => {
 
+    const [userName, setUserName] = useState([])
+    const [topTracks, setTopTracks] = useState([])
+    const [topArtists, setTopArtists] = useState([])
+    const [history, setHistory] = useState([])
+
+    useEffect(() => {
+        UserName()
+        TopTracks()
+        TopArtists()
+        History()
+    }, []);
+
+    const UserName = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get("https://api.spotify.com/v1/me", {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            }
+
+        })
+        console.log(data)
+        setUserName(data.display_name)
+    }
+    const TopTracks = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            },
+            params: {
+                limit: 5
+            }
+
+        })
+        console.log(data)
+        setTopTracks(data.items)
+    }
+
+    const renderTopTracks = () => {  //can only render first aritst's top tracks
+        return topTracks.map((topTrack, index) => (
+            <ListGroup >
+                <ListGroup.Item
+                    className="item"
+                >
+                    <div><img src={topTrack.album.images[0].url} className="cover" alt='artist_image' width='90' height='90' /></div>
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold information-horizontal-center">{topTrack.name}</div>
+                        <div className="information-horizontal-center2">{topTrack.artists[0].name}</div>
+                    </div>
+                    {/* <div><FaPlusSquare className="add-track-button" /></div> */}
+                </ListGroup.Item>
+            </ListGroup>
+        ))
+    }
+
+    const TopArtists = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get("https://api.spotify.com/v1/me/top/artists", {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            },
+            params: {
+                limit: 5
+            }
+
+        })
+        console.log(data)
+        setTopArtists(data.items)
+    }
+
+    const renderArtists = () => {
+        return topArtists.map((topArtist, index) => (
+            <ListGroup>
+                <ListGroup.Item
+                    className="item"
+                >
+                    <div><img src={topArtist.images[0].url} className="cover-circle" alt='artist_image' width='100' height='100' /></div>
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold information-horizontal-center">{topArtist.name}</div>
+                        <div className="information-horizontal-center">{topArtist.genres[0]}</div>
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
+        ))
+    }
+    const History = async (e) => {
+        //e.preventDefault()
+        const { data } = await axios.get("https://api.spotify.com/v1/me/player/recently-played", {
+            headers: {
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            },
+            params: {
+                limit: 5
+            }
+
+        })
+        console.log(data)
+    }
+
     return (
         <div>
+            <header><Appbar /></header>
+            <h3 className="username">{userName}</h3>
             <div>
-                <Image src={Cookies.get('image')} roundedCircle width={130} height={130} />
-                <div
-                    classNeam="username"
-                    style={{
-                        position: "absolute",
-                        top: "10px",
-                        right: "0",
-                        width: "1070px",
-                        height: "80px",
-                        lineHeight: "10px",
-                        fontFamily: "Arial"
-                    }}
-                >
-                    <h3>{Cookies.get('username')}</h3>
-                    <div
-                        classNeam="userInfo"
-                        style={{
-                            position: "absolute",
-                            top: "45px",
-                            right: "0",
-                            width: "1070px",
-                            height: "20px",
-                            lineHeight: "0px",
-                            fontFamily: "Arial"
-                        }}
-                    >
-                        <h4>Favorite Genre: Pop</h4>
-                        <h4>Recently played</h4>
-                    </div>
-                </div>
-                <div
-                    className="TopSongs"
-                    style={{
-                        position: "absolute",
-                        right: "1090px",
-                        top: "130px",
-                        fontFamily: "Arial"
-                    }}
-                >
-                    <h1>Top Songs</h1>
-                </div>
-                <div
-                    classNeam="topsong1"
-                    style={{
-                        position: "absolute",
-                        top: "200px",
-                        right: "1000px",
-                        width: "170px",
-                        height: "170px",
-                        border: "3px solid #DDDDDD",
-                        borderRadius: "10px",
-                        fontFamily: "Arial"
-                    }}
-                >
-                    <img
-                        style={{
-                            width: "100px",
-                            height: "100px",
-                            position: "absolute",
-                            left: "40px",
-                            top: "10px"
-                        }}
-                        src="https://www.iphonefaq.org/files/styles/large/public/apple_music.jpg"
-                    />
-                    <h4
-                        style={{
-                            position: "relative",
-                            left: "50px",
-                            top: "95px"
-                        }}
-                    >
-                        song name
-                    </h4>
-                    <h4
-                        style={{
-                            position: "relative",
-                            left: "60px",
-                            top: "80px"
-                        }}
-                    >
-                        Artist
-                    </h4>
-                    <Button>+</Button>
-                </div>
-                <div
-                    classNeam="topsong2"
-                    style={{
-                        position: "absolute",
-                        top: "200px",
-                        right: "750px",
-                        width: "170px",
-                        height: "170px",
-                        border: "3px solid #DDDDDD",
-                        borderRadius: "10px",
-                        fontFamily: "Arial"
-                    }}
-                >
-                    <img
-                        style={{
-                            width: "100px",
-                            height: "100px",
-                            position: "absolute",
-                            left: "40px",
-                            top: "10px"
-                        }}
-                        src="https://www.iphonefaq.org/files/styles/large/public/apple_music.jpg"
-                    />
-                    <h4
-                        style={{
-                            position: "relative",
-                            left: "50px",
-                            top: "95px"
-                        }}
-                    >
-                        song name
-                    </h4>
-                    <h4
-                        style={{
-                            position: "relative",
-                            left: "60px",
-                            top: "80px"
-                        }}
-                    >
-                        Artist
-                    </h4>
-                </div>
+                <Image src={Cookies.get('image')} roundedCircle width={110} height={110} className="profileImg" />
             </div>
+            <h3 className="profileImg">Top Songs</h3>
+            <ListGroup horizontal className="toptracks-list" >
+                {renderTopTracks()}
+            </ListGroup>
+            <h3 className="profileImg">Top Artists</h3>
+            <ListGroup horizontal className="toptracks-list">
+                {renderArtists()}
+            </ListGroup>
         </div>
     );
 };
