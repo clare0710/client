fff99fea67cb3a47165ebd19df4a220470e2ceb2
clare0710 <lisanwan710@gmail.com>
2023-01-04 23:46:28 +0800

    Modify Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             <p className="text-center">
                 S•poll•tify
             </p>
-            <a className='button-center' href='http://localhost:3001/api/v1/sign_in'><Button background-color='#4CAF50'>
+            <a className='button-center' href='http://44.206.233.63/api/v1/sign_in'><Button background-color='#4CAF50'>
                 <img className='.button-center ' src={SpotifyImg} width="30" alt='spotify_icon' /> Sign in with Spotify
             </Button></a>
         </header>
