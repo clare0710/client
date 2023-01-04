35191cdc0a9440e3dee14d1984b6f89bb09a1c34
clare0710 <lisanwan710@gmail.com>
2023-01-04 11:05:49 +0800

    Build product

---
+++ build/precache-manifest.7487aaf248eb14b4bcb712076e75dbc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "603c8fbd3827d2f7672a",
+    "url": "/static/css/main.029794b2.chunk.css"
+  },
+  {
+    "revision": "603c8fbd3827d2f7672a",
+    "url": "/static/js/main.aa66c543.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23ef30a5bfe1547540f9",
+    "url": "/static/css/2.25270389.chunk.css"
+  },
+  {
+    "revision": "23ef30a5bfe1547540f9",
+    "url": "/static/js/2.d0371876.chunk.js"
+  },
+  {
+    "revision": "e16076fdb458b65b8d9d99c496ab39b6",
+    "url": "/static/media/vertical_menu.e16076fd.svg"
+  },
+  {
+    "revision": "4f19bf8da781e933d686c789df5674bc",
+    "url": "/static/media/spotify_Icon.4f19bf8d.png"
+  },
+  {
+    "revision": "0c961829e4d60bbe65deb842b45db737",
+    "url": "/index.html"
+  }
+];
